13fc745a1764913bcc11571ebc3eadec280b0107
Vassilis Kolokithas <vassiliskolokithas@shobu.local>
2025-03-27 00:00:01 +0200

    pagination fix limit

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -30,7 +30,7 @@ class ClientController extends Controller
         // Φορτώνουμε τα renewal logs μαζί με τα clients για καλύτερη απόδοση
         $clients = $query->with('renewalLogs')
                         ->orderBy('hosting_expiration_date', 'asc')
-                        ->paginate(1);
+                        ->paginate(10);
 
         return view('clients.index', compact('clients'));
     }
